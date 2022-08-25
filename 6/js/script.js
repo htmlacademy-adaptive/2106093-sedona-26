7d630ab13ccbbd3d5fdd8abf4cbf05466360c0cc
Keks <keks@htmlacademy.ru>
2022-08-25 19:24:23 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/script.js
@@ -0,0 +1,19 @@
+let navList = document.querySelector('.nav-list');
+let navToggleOpen = document.querySelector('.main-nav__toggle-open');
+let navToggleClose = document.querySelector('.main-nav__toggle-close');
+
+navList.classList.remove('nav-list--nojs');
+
+navToggleOpen.addEventListener('click', function () {
+  if (navList.classList.contains('nav-list--close')) {
+    navList.classList.remove('nav-list--close');
+    navList.classList.add('nav-list--open');
+  }
+});
+
+navToggleClose.addEventListener('click', function () {
+  if (navList.classList.contains('nav-list--open')) {
+    navList.classList.remove('nav-list--open');
+    navList.classList.add('nav-list--close');
+  }
+});
