d65fed09438b6b594d119f01a8267f97d5873ab0
ivan_duzhak <ivanduzhak@gmail.com>
2022-09-19 15:29:30 +0300

    Редактирование ошибок стайлинт

--- source/js/map.js
+++ source/js/map.js
@@ -1,8 +1,8 @@
-// let mapOn = document.querySelector('.map__online');
-// let mapOff = document.querySelector('.map__offline');
+let mapOn = document.querySelector('.map__online');
+let mapOff = document.querySelector('.map__offline');
 
-// mapOn.classList.remove('map__online--nojs');
-// mapOff.classList.add('map__offline--js');
+mapOn.classList.remove('map__online--nojs');
+mapOff.classList.add('map__offline--js');
 
 
 
